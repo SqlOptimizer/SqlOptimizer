d0e4c28570f45fc0c5e2dd67bddec8caa3b12570
san1991 <syq3b@mst.edu>
2014-11-30 12:17:54 -0600

    1)Added comments to Node.java

--- src/Node.java
+++ src/Node.java
@@ -1,65 +1,21 @@
 /**
- * Authors: Shen Yeung and Katrina Ward
- * CS 5300 Fall 2014 Project
+ * Created by Shen on 11/2/2014.
  */
 import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
-/**
- * Class:    Node
- * Purpose:  Used to represent a Node in a QueryTree
- *
- * Methods:
- * 
- * Node(ArrayList<Tuple>, String)
- * Description: Constructor
- * Pre: Name needs to include one of the relational algebra keywords. SELECT, JOIN, etc
- * Post: Will create a new node that contains the given name and data
- * Param 1: Data for the new node
- * Param 2: Name for the node
- * 
- * Node(Node)
- * Description: Copy Constructor
- * Pre: None
- * Post: Will create a new node that is an exact copy of another node
- * Param: Node that will be copied
- * 
- * Get/Set ....
- * Description: Accessors and Mutators
- * Pre: None
- * Post: Will return or set the values inside the node
- * 
- * Insert(Node)
- * Description: Adds a node to this node.
- * Pre: This node must have the left or right child set to null
- * Post: Will attach a node, checking left first, then right. New node is a copy of the node given.
- * 
- * Insert(Node, Node)
- * Description: Adds two nodes to the current node
- * Pre: This node must have both children set to null
- * Post: Will attach two nodes to the current node. These nodes will be copies of the passed in nodes
- * Param 1: Node to be attached to the left
- * Param 2: Node to be attached to the right
- * 
- * 
- * 
- * NEEDS COMPLETED
- */
+/**********************************************************/
+// Class Node for representing each node in the Query-tree./
+/**********************************************************/
 
 public class Node{
-    /**************************************************************************************************/
-    /*                 Member Variables                                                                               */
-    /**************************************************************************************************/
     ArrayList<Tuple<String, String>> data;     // < name, alias> 
     private Node parent;
     private Node leftChild;
     private Node rightChild;
     private String name;            // Project, Join, ... etc
 
-    /**************************************************************************************************/
-    /*                 Member Methods                                                                 */
-    /**************************************************************************************************/
     //Node Default Constructor
     public Node(){
         name = "null";
@@ -70,19 +26,23 @@ public class Node{
     }
 
     //constructor taking in data and name to assign to a node
-    public Node(ArrayList< Tuple<String, String> > newData, String newName){
-        this.name = new String(newName);
-        this.data = new ArrayList<Tuple<String, String>>(newData);
+    public Node(ArrayList< Tuple<String, String> > data, String name){
+        this.setName(name);
+        this.setData(data);
     }
 
     //copy constructor
     public Node(Node node){
-        data = new ArrayList<Tuple<String, String>>(node.data);
-        parent = new Node(node.parent);
-        leftChild=new Node(node.leftChild);
-        rightChild= new Node(node.rightChild);
-        name = new String(node.name);
+        this.setData(node.data);
+        this.setParent(node.parent);
+        this.setLeftChild(node.leftChild);
+        this.setRightChild(node.rightChild);
+        this.setName(node.name);
     }
+
+    /****************************************/
+    //        Gets & Sets Functions          //
+    /****************************************/
 
     public ArrayList<Tuple<String, String>> getData(){
         return this.data;
@@ -124,7 +84,9 @@ public class Node{
         this.name = name;
     }
 
-    //inserting a new node to the current node
+    //This insert method is used when only inserting a SINGLE node into the tree.
+    //This implies that the inserted node is NOT a join node; else you would use the other version of insert function.
+    //The inserted node will ALWAYS be inserted to the LEFT child of the last node currently resides in the tree.
     public void insert(Node relation) {
         //if the current node has no children, then assign it to the left child
         if(this.leftChild == null){
@@ -137,8 +99,12 @@ public class Node{
         }
     }
 
-    //this insert function is used to insert two nodes
+    //This insert function is used to insert two nodes
     //mainly used for inserting children under a "JOIN" node
+    //Pre: The last node of the current tree has NO left or right child
+    //Pro: The last node of the current tree will have BOTh left and right child
+    //Case 1: Both left and right child are Relation
+    //Case 2: Left child is JOIN, while right child is Relation (to preserve right linear property)
     public void insert(Node relation, Node relation1) {
         //if current node is null, then insert to both left and right children
         if(this.leftChild == null){
@@ -146,20 +112,6 @@ public class Node{
             this.rightChild = relation1;
             relation.setParent(this);
             relation1.setParent(this);
-
-//            //stored the table origin information
-//            if(relation.getData().size() != 0){
-//                this.getData().add(relation.getData().get(0));
-//
-//                //add the value to the join node parent
-//                Node parent = this.getParent();
-//                while(parent.getName() == "JOIN"){
-//                    if(!parent.getData().contains(this.getData().get(0))){
-//                        parent.getData().add(this.getData().get(0));
-//                    }
-//                    parent = parent.getParent();
-//                }
-//            }
         }
         else{
             //traverse to a node whose left child is null
@@ -167,33 +119,36 @@ public class Node{
         }
     }
 
+    //A function to be used to perform join after all other operations have been performed in the QueryTree
+    //Pre: Tree constructed all operations besides join
+    //Pro: Tree modified with additional representations for join nodes
     public void performJoin(query newQuery) {
         int i = newQuery.relations.size();
-        //ArrayList<Tuple<String, String>> init = new ArrayList<Tuple<String, String>>(newQuery.relations);
-        //init.remove(i-1);
-        //this.insert(new Node(init, "JOIN"));
 
+        //initializing the first join node and insert into the tree
         ArrayList<Tuple<String, String>> init = new ArrayList<Tuple<String, String>>();
         init.add(new Tuple<String, String>("null", "null"));
         this.insert(new Node(new ArrayList<Tuple<String, String>>(init), "JOIN"));
 
+        //perform join operations and insertions into the tree given the number of relations size
         while(i >= 2){
+            //if the number of relations exceeds 2, then perform join in the form of "Join, Relation"
+            //in which join node insert to the left, and Relation insert to the right to preserve right-linear property
             if(i > 2){
                 ArrayList<Tuple<String, String>> list = new ArrayList<Tuple<String, String>>();
                 list.add(newQuery.relations.get(i-1));
 
-//                ArrayList<Tuple<String, String>> listTwo = new ArrayList<Tuple<String, String>>(newQuery.relations);
-//                listTwo.remove(i-1);
-
-//                this.insert(new Node(new ArrayList<Tuple<String, String>>(listTwo), "JOIN"),
-//                        new Node(new ArrayList<Tuple<String, String>>(list), "RELATION"));
                 init.clear();
                 init.add(new Tuple<String, String>("null", "null"));
+
+                //insert into the tree
                 this.insert(new Node(new ArrayList<Tuple<String, String>>(init), "JOIN"),
                         new Node(new ArrayList<Tuple<String, String>>(list), "RELATION"));
-                i=i-1;
+
+                i=i-1; //decrease the number of relation
             }
             else{
+                //only two relations left
                 ArrayList<Tuple<String, String>> listOne = new ArrayList<Tuple<String, String>>();
                 listOne.add(newQuery.relations.get(0));
 
@@ -207,6 +162,9 @@ public class Node{
         }
     }
 
+    //This function is similar to the previous join performing function
+    //With the exception that this handles queries that contain a sub-query
+    //The logics is closely similar with the previous function
     public void performJoinWithSubquery(query newQuery) {
         //First, create a tree for the subquery
         QueryTree sub = new QueryTree();
@@ -214,22 +172,18 @@ public class Node{
 
         int i = newQuery.relations.size();
 
+        //initializing the first join node
         ArrayList<Tuple<String, String>> init = new ArrayList<Tuple<String, String>>();
         init.add(new Tuple<String, String>("null", "null"));
         this.insert(new Node(new ArrayList<Tuple<String, String>>(init), "JOIN"));
 
+        //if the query contains only one relation, simply join it with the sub-query
         if(i == 1){
-//            this.insert(new Node(new ArrayList<Tuple<String, String>>(), "JOIN"));
-            init.clear();
-            init.add(new Tuple<String, String>("null", "null"));
-            this.insert(new Node(init, "JOIN"));
             ArrayList<Tuple<String, String>> list = new ArrayList<Tuple<String, String>>();
             list.add(newQuery.relations.get(0));
             this.insert(new Node(new ArrayList<Tuple<String, String>>(list), "RELATION"), sub.getRoot());
         }
         else{
-//            this.insert(new Node(newQuery.relations, "JOIN"));
-//            this.insert(new Node(new ArrayList<Tuple<String, String>>(), "JOIN"), sub.getRoot());
             init.clear();
             init.add(new Tuple<String, String>("null", "null"));
             this.insert(new Node(new ArrayList<Tuple<String, String>>(init), "JOIN"), sub.getRoot());
@@ -251,6 +205,7 @@ public class Node{
                     listOne.add(newQuery.relations.get(0));
                     ArrayList<Tuple<String, String>> listTwo = new ArrayList<Tuple<String, String>>();
                     listTwo.add(newQuery.relations.get(i-1));
+
                     this.insert(new Node(new ArrayList<Tuple<String, String>>(listOne), "RELATION"),
                             new Node(new ArrayList<Tuple<String, String>>(listTwo), "RELATION"));
                     i = i-2;
@@ -260,9 +215,13 @@ public class Node{
     }
 
     //used to print information for outputing the .gv extension file
+    //Pre: Given the int i representing the current 'id' or index of the node
+    //Pro: output a string containing the given index, and appended appropriate label
     public String print(int i) {
         String line = "node" + Integer.toString(i);
         line = line + "[ label = \"";
+
+        //check for the name of the node and append appropriate info
         if(!this.getName().contentEquals("JOIN")){
             //break down into different category
             if(this.getName().contentEquals("PROJECT")){
@@ -279,17 +238,19 @@ public class Node{
             }
         }
         else{
+            //if the node is "JOIN", then check to see whether it's Cartesian Product or Theta-Join
+            //thus give appropriate label
             if(this.getName().contentEquals("JOIN") && !this.getData().get(0).getLeft().contentEquals("null")){
                 line = line + "JOIN" + "( " + tupleToString(this.getData()) + " )\" ]";
             }
             else{
                 line = line + "X" + "\"]";
             }
-
         }
         return line;
     }
 
+    //Convert relation information to be string to be used in the Print function
     private String relationToString(ArrayList<Tuple<String, String>> data) {
         if(data.get(0).getRight().contentEquals("null")){
             return data.get(0).getLeft()+ " ";
@@ -312,59 +273,4 @@ public class Node{
         }
         return line;
     }
-
-//    public int outputSubquery(int i, String filePath, boolean append)throws IOException{
-//        WriteFile writer = new WriteFile(filePath, append);
-//
-//        //traverse the tree
-//        String line = new String();
-//
-//        //index to denote the number of node (to output to the file)
-//        int j = i;
-//
-//        Node node = this;
-//        line = node.print(j);
-//        writer.writeToFile(line);
-//        node = node.getLeftChild();
-//        while(node != null){
-//            if(node.getName() != "JOIN"){
-//                if(node.getParent().getName() != "JOIN"){
-//                    line = node.print(++j);
-//                    writer.writeToFile(line);
-//                    writer.writeToFile("node" + Integer.toString(j-1) + "->" + "node" + Integer.toString(j));
-//                    node = node.getLeftChild();
-//                }
-//                else{
-//                    node = node.getLeftChild();
-//                    j++;
-//                }
-//            }
-//            else{
-//                if(node.getParent().getName() != "JOIN"){
-//                    line = node.print(++j);
-//                    writer.writeToFile(line);
-//                    writer.writeToFile("node" + Integer.toString(j-1) + "->" + "node" + Integer.toString(j));
-//                    line = node.getRightChild().print(j+1);
-//                    writer.writeToFile(line);
-//                    writer.writeToFile("node" + Integer.toString(j) + "->" + "node" + Integer.toString(j+1));
-//                    line = node.getLeftChild().print(j+2);
-//                    writer.writeToFile(line);
-//                    writer.writeToFile("node" + Integer.toString(j) + "->" + "node" + Integer.toString(j+2));
-//                    node = node.getLeftChild();
-//                    j = j+2;
-//                }
-//                else{
-//                    line = node.getRightChild().print(j+1);
-//                    writer.writeToFile(line);
-//                    writer.writeToFile("node" + Integer.toString(j) + "->" + "node" + Integer.toString(j+1));
-//                    line = node.getLeftChild().print(j+2);
-//                    writer.writeToFile(line);
-//                    writer.writeToFile("node" + Integer.toString(j) + "->" + "node" + Integer.toString(j+2));
-//                    node = node.getLeftChild();
-//                    j = j+2;
-//                }
-//            }
-//        }
-//        return j;
-//    }
 }
